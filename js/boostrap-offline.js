432e84b81c7c429ed5b878ea4e61679ecff399aa
ByAlexCG <sottoderly@gmail.com>
2021-10-25 17:41:48 -0500

    Boostrap Offline

---
+++ js/boostrap-offline.js
@@ -0,0 +1,34 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+   
+    window.addEventListener('load', function(){
+      /*   Alert */
+      var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+      var alertTrigger = document.getElementById('liveAlertBtn')
+      
+      function alert(message, type) {
+        var wrapper = document.createElement('div')
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+      
+        alertPlaceholder.append(wrapper)
+      }
+      
+      if (alertTrigger) {
+        alertTrigger.addEventListener('click', function () {
+          alert('Alerta generada por Boostrap!', 'success')
+        })
+      }
+
+      /* validacion */
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  });

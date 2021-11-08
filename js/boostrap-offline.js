689fb108d3673560ab9952ebc513032da8a7d96d
Derly Soto <sottoderly@gmail.com>
2021-11-08 17:27:27 -0500

    Agregando Boostrap Offline

--- js/boostrap-offline.js
+++ js/boostrap-offline.js
@@ -32,3 +32,27 @@
         }, false)
       })
   });
+window.addEventListener("load", function(){ 
+    /*  Plugin Tooltip */
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    /* Pluion de Popers */
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+
+    /*  Pluii Toast */
+    var toastTrigger = document.getElementById('liveToastBtn')
+      var toastLiveExample = document.getElementById('liveToast')
+      if (toastTrigger) {
+        toastTrigger.addEventListener('click', function () {
+          var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+});

00bc0ac04139e3f9b843da301ce9ed70ba80dc4b
Derly <sottoderly@gmail.com>
2022-02-08 17:33:43 -0500

    Verificando actividad

---
+++ javascript/js/26-eventosFields.js
@@ -0,0 +1,6 @@
+
+function validarInfo(numeroDocumento) {
+    let numeroDoc = document.getElementById(numeroDocumento).value;
+    if (numeroDoc === "")
+    alert('ALERTA!! El numero de documento no puede estar vacio')
+}

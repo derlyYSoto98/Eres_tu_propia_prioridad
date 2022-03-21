de8e3f147392afa92e01a882bd3b97f0d4715be7
Derly Soto <sottoderly@gmail.com>
2022-03-20 22:17:02 -0500

    Buscador mercado libre

---
+++ javascript/js/buscar_productos_javascript.js
@@ -0,0 +1,30 @@
+"use strict"
+class Automovil{
+    constructor(marca, modelo, anyo){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anyo = anyo;
+    }
+}
+
+let auto = new Automovil("Toyota", "txl", 2021);
+
+function mostrarVehiculo(){
+    // alert(auto.marca +" " + auto.modelo);
+    let Contenedor = document.createElement("div");
+    let contenMain = document.getElementById("main_content");
+    contenMain.appendChild(Contenedor);
+
+    let EtiquetaAuto = document.createElement("label");
+    Contenedor.appendChild(EtiquetaAuto);
+
+    let txtMarca = document.createTextNode(auto.marca);
+    EtiquetaAuto.appendChild(txtMarca);
+
+    let txtModelo = document.createTextNode(auto.modelo);
+    EtiquetaAuto.appendChild(txtModelo);
+
+    let txtYears = document.createTextNode(auto.anyo);
+    EtiquetaAuto.appendChild(txtYears);
+
+}
